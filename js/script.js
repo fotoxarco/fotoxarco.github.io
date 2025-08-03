cde42325e165d580f62c130b0b81897557a86bad
fotoxarco <salkalero@gmail.com>
2025-08-04 00:27:49 +0200

    eliminar js

--- js/script.js
+++
@@ -1,6 +0,0 @@
-
-includes/navigator.html // o 'includes/nav.html' si lo metes en carpeta  ofetch('script.html')
-    .then(response => response.text())
-    .then(data => {
-        document.getElementById('script-placeholder').innerHTML = data;
-    });
